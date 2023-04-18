0350f401e68d583e57ccdb262020645b630a2c6c
Nikolay Naumenko <nnv2006@ukr.net>
2023-04-18 22:31:39 +0300

    auth and pagination done

---
+++ controllers/auth.js
@@ -0,0 +1,77 @@
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { User } = require("../models/user");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+const { SECRET_KEY } = process.env;
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  console.log("email:", email);
+  console.log("password:", password);
+
+  const user = await User.findOne({ email });
+  console.log("user:", user);
+
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  console.log("passwordCompare:", passwordCompare);
+
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = { id: user._id };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  console.log("token:", token);
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({ token });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  console.log("_id:", _id);
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204);
+  // res.json({ message: "logout sucsess" });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+};
